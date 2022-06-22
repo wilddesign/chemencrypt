20e498ffdba789bbb0684e91c6e9465ba24099ef
Wojciech Nogaś <wnappdv@gmail.com>
2022-06-22 21:23:19 +0200

    stats calculation under way

--- src/encryption.ts
+++ src/encryption.ts
@@ -1,17 +1,23 @@
 import {Property, Chemical, ComponentChemical, MixtureOfChemicals} from './index'
 import {Config, BasicCalculations, BasicSearches, BasicSelects } from './calculations'
+import {SetMeasures} from './setmeasures'
+
 
 export class PlausiblyDeniableChemicalEncryptionConfigs {
   isomerConfig: string
   propertyConfig: string
   numberOfCycles: number
   propertiesInterfered: string[]
-  constructor(isomerConfig: 'all'|'single', propertyConfig: 'all'|'single', numberOfCycles: number, propertiesList: string[]){
+  constructor(isomerConfig: string, propertyConfig: string, numberOfCycles: number, propertiesList: string[]){
     this.isomerConfig = isomerConfig
     this.propertyConfig = propertyConfig
     this.numberOfCycles = Math.floor(numberOfCycles)
     this.propertiesInterfered = propertiesList
   }
+}
+
+type encryptionStats = {
+  measure: number
 }
 
 export class PlausiblyDeniableChemicalEncryption {
@@ -53,7 +59,7 @@ export class PlausiblyDeniableChemicalEncryption {
           escrow = escrow.concat(BasicSelects.selectOneRandomly(isomers))
         } else if (this.encryptParams.isomerConfig === 'all') {
           escrow = escrow.concat(BasicSelects.selectAll(isomers))
-        }
+        } else if (this.encryptParams.isomerConfig === 'none') {}
         if(escrow.length){
           this.moveChemicalsFromAvailableChemicalsToToBeEncrypted(escrow)
         }
@@ -63,6 +69,7 @@ export class PlausiblyDeniableChemicalEncryption {
           escrow = []
           //find the property in this.encryptedTypeChemical
           let propOfInterest: Property = encryptedTypeChemical.properties.filter((prop) => {return prop.name === param})[0]
+
           let configFittingTheParam: Config = this.configs.filter((conf) => {return conf.propertyName === param})[0]
           if (configFittingTheParam !== undefined && propOfInterest !== undefined){
             let interferents: Array<Chemical> = BasicSearches.findAllChemicalsByPropCondition(propOfInterest, this.availableChemicals, configFittingTheParam)
@@ -117,6 +124,14 @@ export class PlausiblyDeniableChemicalEncryption {
     })
 
     this.availableChemicals.splice(index,1)
+  }
+
+  calculateStats(propertyNames: Config[]) {
+    //for each propertyName signal in the analyte, calculate the
+    // Lebesgue measures that pertain to the encryption efficiency,
+    // namely, overlap of analyte and interferents signals
+    // take epsilon from this.configs
+    return this.toBeEncrypted
   }
 
 }
---
+++ src/setmeasures.ts
@@ -0,0 +1,36 @@
+
+type multipleOverlap = {
+  sumOfOverlapAreas: number
+  numberOfOverlappingSignals: number
+}
+
+export class SetMeasures {
+
+    static calculateOverlapAreaOfTwoSignals(epsilon: number, signal1: number, signal2: number): number{
+      let max = this.signalArea(epsilon)
+      if(Math.abs(signal1-signal2) > 2*epsilon) {
+        return 0
+      } else {
+        return max - Math.abs(signal1-signal2)
+      }
+    }
+
+    static signalArea(epsilon: number): number{
+      return 2*epsilon
+    }
+
+    static calculateOverlapsOfASignalWithMultipleSignals(epsilon: number, singleSignal: number, multipleSignals: number[]): multipleOverlap{
+      let result = {
+        sumOfOverlapAreas: 0,
+        numberOfOverlappingSignals: 0
+      }
+      multipleSignals.forEach(signalFromMultiple => {
+        let overlap = this.calculateOverlapAreaOfTwoSignals(epsilon, singleSignal, signalFromMultiple)
+        if (overlap > 0) {
+          result.sumOfOverlapAreas += overlap
+          result.numberOfOverlappingSignals++
+        }
+      });
+      return result
+    }
+}
